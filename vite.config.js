4b64473aadff67fa3fd4b6a30d4790be285596fe
Tommy Feldt <tommyfeldt@gmail.com>
2025-04-05 23:20:34 +0200

    fix issue with asset serving

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   root: "src",
+  base: command === "serve" ? "/" : "/signalk-heel-angle/",
   server: {
     host: true,
     port: 5173,
@@ -10,11 +11,11 @@ export default defineConfig({
   build: {
     outDir: "../public",
     sourcemap: true,
-    assetsDir: ".",
+    assetsDir: "assets",
     emptyOutDir: true,
   },
   assetsInclude: ["**/*.svg"],
   define: {
     "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV),
   },
-});
+}));
